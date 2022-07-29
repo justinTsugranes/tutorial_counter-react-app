51faa89af29d794d016ad26163b360772310145c
justintsugranes <justintsugranes@proton.me>
2022-07-29 08:32:25 -0600

    fixed errors in curly bracketing

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
     return (
       <div>
-      <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-      <button onClick={() => this.handleIncrement({ id: 1 }) }
-      className='btn btn-secondary btn-sm'>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
-  };
-}
+  }
 
-getBadgeClasses() {
-  let classes = "badge m-2 badge-";
-  classes += this.state.count === 0 ? "warning" : "primary";
-  return classes;
-}
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  formatCount() {
+    const { value } = this.propers.counter;
+    return value === 0 ? "Zero" : value;
+  }
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,14 +2,45 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {};
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    console.log(counter);
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id != counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
+        <button className="btn btn-primary btn-sm m-2">Reset</button>
+        onClick={this.handleReset}
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+            id={counter.id}
+          />
+        ))}
       </div>
     );
   }
